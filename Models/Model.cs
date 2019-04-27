cae79281941e305708cd7de323e1169d2b478016
Andrew Harasivka <andriygarasivka@gmail.com>
2019-04-28 00:51:28 +0300

    Added constraints and migration

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,8 +1,15 @@
-﻿namespace vega.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vega.Models
 {
+    [Table("Models")]
     public class Model
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
 
         public string Name { get; set; }
 
