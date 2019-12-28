249c12d8c949cbfe394aa02f3acb60fc3e677f61
Andrew Harasivka <andriygarasivka@gmail.com>
2019-12-29 01:41:40 +0200

    added new migration

--- vega/Migrations/20191228233915_ApplyConstraints.Designer.cs
+++ vega/Migrations/20191228233915_ApplyConstraints.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vega.Persistance;
 
 namespace vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191228233915_ApplyConstraints")]
+    partial class ApplyConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- vega/Models/Make.cs
+++ vega/Models/Make.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
@@ -8,6 +9,8 @@ namespace vega.Models
     {
         public int Id { get; set; }
         
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public ICollection<Model> Models { get; set; }
