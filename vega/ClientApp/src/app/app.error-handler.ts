f6fcafa633b561d003c106e8890e024d713a2adb
Andrew Harasivka <andriygarasivka@gmail.com>
2020-01-11 21:42:28 +0200

    added error handler with sentry logging

---
+++ vega/ClientApp/src/app/app.error-handler.ts
@@ -0,0 +1,30 @@
+import { ToastyService } from 'ng2-toasty';
+import { ErrorHandler, Inject, NgZone, isDevMode } from "@angular/core"
+import * as Sentry from '@sentry/browser';
+
+export class AppErrorHandler implements ErrorHandler {
+    constructor(
+        private ngZone: NgZone,
+        @Inject(ToastyService) private toastyService: ToastyService) {
+
+    }
+
+    handleError(error: any): void {
+        if (!isDevMode()) {
+            const eventId = Sentry.captureException(error.originalError || error);
+            Sentry.showReportDialog({ eventId });
+        }
+        else
+            throw error;
+
+        this.ngZone.run(() => {
+            this.toastyService.error({
+                title: 'Error',
+                msg: 'An unexpected error happened.',
+                theme: 'bootstrap',
+                showClose: true,
+                timeout: 5000
+            });
+        });
+    }
+}
--- vega/ClientApp/src/app/app.module.ts
+++ vega/ClientApp/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable, isDevMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { ToastyModule } from 'ng2-toasty';
 import { VehicleService } from "./services/VehicleService";
+import * as Sentry from '@sentry/browser';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -13,6 +14,11 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { AppErrorHandler } from './app.error-handler';
+
+Sentry.init({
+  dsn: "https://0df0c7e86716463891cfe42dffb9130b@sentry.io/1878365"
+});
 
 @NgModule({
   declarations: [
@@ -37,7 +43,8 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
     ])
   ],
   providers: [
-    VehicleService
+    VehicleService,
+    { provide: ErrorHandler, useClass: AppErrorHandler }
   ],
   bootstrap: [AppComponent]
 })
