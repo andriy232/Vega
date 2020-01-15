6ff33979a4e3b5e0a53d614c61645f80b344b620
Andrew Harasivka <andriygarasivka@gmail.com>
2020-01-15 23:51:15 +0200

    added button groups

--- vega/ClientApp/src/app/view-vehicle/view-vehicle.component.ts
+++ vega/ClientApp/src/app/view-vehicle/view-vehicle.component.ts
@@ -2,12 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { VehicleService } from "../services/VehicleService";
 import { ToastyService } from 'ng2-toasty';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 import 'rxjs/add/observable/forkJoin';
-import { SaveVehicle } from '../models/SaveVehicle';
-import { Vehicle } from "../models/Vehicle";
 import * as _ from 'underscore';
-import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-view-vehicle',
@@ -16,7 +12,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 })
 export class ViewVehicleComponent implements OnInit {
   vehicle: any;
-  vehcileId: number;
+  vehicleId: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,8 +21,8 @@ export class ViewVehicleComponent implements OnInit {
     private vehicleService: VehicleService) {
 
     route.params.subscribe(p => {
-      this.vehcileId = +p['id'];
-      if (isNaN(this.vehcileId) || this.vehcileId <= 0) {
+      this.vehicleId = +p['id'];
+      if (isNaN(this.vehicleId) || this.vehicleId <= 0) {
         router.navigate(['/vehicles']);
         return;
       }
@@ -34,8 +30,10 @@ export class ViewVehicleComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.vehicleService.getVehicle(this.vehcileId)
-      .subscribe(v => this.vehicle = v, err => {
+    this.vehicleService.getVehicle(this.vehicleId)
+      .subscribe(v => {
+        this.vehicle = v;
+      }, err => {
         if (err.status == 404) {
           this.router.navigate(['/vehicles']);
           return;
